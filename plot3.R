3970c4c288d0a791eb5c84c2efd04ddf98adcc4f
Abraham Aizenman <aaizenm@gmx.com>
2014-07-09 21:45:09 +0300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -4,7 +4,7 @@ lines(hpc2$Sub_metering_2, col="red")
 lines(hpc2$Sub_metering_3, col="blue")
 axis(1, at=c(match("2007-01-02 00:00:00",hpc2$NewDate),match("2007-02-02 00:00:00",hpc2$NewDate),match("2007-02-02 23:59:00",hpc2$NewDate)), labels=c("Thu", "Fri", "Sat"))
 # add legend
-legend("topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), title="", lwd=1, x.intersp=.01, y.intersp=.75, cex=.75)
+legend(1950,29, c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), title="", lwd=1, x.intersp=.8, y.intersp=.82, cex=.75, xjust=0, yjust=0, text.width=720)
 
 dev.copy(png, file="plot3.png", width = 480, height = 480, units = "px")
 dev.off()
